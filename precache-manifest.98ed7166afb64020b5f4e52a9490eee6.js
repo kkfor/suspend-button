6c3dd6cbe76c4dc9c0ccb0d7a5ef2606ee2067e6
wangyaqi <wangyaqi@xfabcd.com>
2019-04-04 16:20:23 +0800

    Updates

---
+++ precache-manifest.98ed7166afb64020b5f4e52a9490eee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/suspendbutton/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6fe1de51618e71d479fc",
+    "url": "/suspendbutton/static/js/runtime~main.9fbe8758.js"
+  },
+  {
+    "revision": "85afd07267436db04b31",
+    "url": "/suspendbutton/static/js/main.c7f461f7.chunk.js"
+  },
+  {
+    "revision": "87df811b76da94a157c1",
+    "url": "/suspendbutton/static/js/2.59f7fd83.chunk.js"
+  },
+  {
+    "revision": "85afd07267436db04b31",
+    "url": "/suspendbutton/static/css/main.e2a26a15.chunk.css"
+  },
+  {
+    "revision": "19c36bd709276c624bd74c72e3b53bae",
+    "url": "/suspendbutton/index.html"
+  }
+];
